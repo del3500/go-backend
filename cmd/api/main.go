e62be39adf4076a50d2388c052c4a9d7c67c388f
del3500 <wendellsmesia@gmail.com>
2024-11-12 23:57:55 +0800

    added config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,14 @@
 package main
 
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"time"
+)
+
 const version = "1.0.0"
 
 type config struct {
@@ -7,5 +16,39 @@ type config struct {
 	env  string
 }
 
+type application struct {
+	config config
+	logger *slog.Logger
+}
+
 func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "Environment (development|staging|production)")
+	flag.Parse()
+
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthCheckHandler)
+
+	srv := &http.Server{
+		Addr:         fmt.Sprintf(":%d", cfg.port),
+		Handler:      mux,
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		ErrorLog:     slog.NewLogLogger(logger.Handler(), slog.LevelError),
+	}
+
+	logger.Info("starting server", "addr", srv.Addr, "env", cfg.env)
+	err := srv.ListenAndServer()
+	logger.Error(err.Error())
+	os.Exit(1)
 }
